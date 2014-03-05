fda4ad6494d2928e4f5c119b64255d7986fdb12a
Bill Walker <bwalker@mozilla.com>
2014-03-05 13:22:15 -0800

    less chatty, more useful for calling from another script

--- find_device_name.sh
+++ find_device_name.sh
@@ -11,9 +11,4 @@ for line in $deviceList; do
 	[[ $line =~ ([0-9]*)device$ ]] && device=${line%%device}
 done
 
-if [ $device == 'unknown' ] ; then
-	echo "device not found"
-	exit 1
-fi
-
 echo $device
