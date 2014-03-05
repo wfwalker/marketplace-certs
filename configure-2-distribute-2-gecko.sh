a5725c527169c6fc118acb38d75eef60c6050cc3
Bill Walker <bwalker@mozilla.com>
2014-03-05 11:51:21 -0800

    require single command-line argument which is hostname use hostname argument when calling change_trusted_servers.sh

--- configure-2-distribute-2-gecko.sh
+++ configure-2-distribute-2-gecko.sh
@@ -4,7 +4,27 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+if [ $# -ne 1 ]; then
+	echo "usage: configure-2-distribute-2-gecko.sh <hostname>"
+	exit 1
+else
+	d2gHostname=$1
+fi
+
 echo "\n*** configure-2-distribute-2-gecko"
+
+echo "\n*** wiping temporary cert DB"
+rm -Rf certdb.tmp
+
+echo "\n*** create new temporary cert DB"
+./new_certdb.sh certdb.tmp
+
+echo "\n*** fetch DER from d2g server"
+echo "wget $d2gHostname/publicKey.der"
+
+echo "\n*** add d2g cert to temporary cert DB"
+# TODO: use the DER generaeted by d2g here
+./add_or_replace_root_cert.sh certdb.tmp marketplace-dev-public-root
 
 echo "\n*** find device name"
 
@@ -16,30 +36,20 @@ IFS='
 device='unknown'
 
 for line in $deviceList; do
-	[[ $line =~ ([0-9]*)device$ ]] && device=${line%%device}
+	[[ $line =~ [0-9]*device$ ]] && device=${line%%device}
 done
 
 if [ $device == 'unknown' ] ; then
-	echo "device not found"
+	echo "Firefox OS device not found."
+	echo "Please connect your device via ADB, turn it on, unlock it, enable remote debugging, and try again"
 	exit 1
 fi
 
 echo "found device $device"
 
-echo "\n*** wiping temporary cert DB"
-rm -Rf certdb.tmp
-
-echo "\n*** create new temporary cert DB"
-./new_certdb.sh certdb.tmp
-
-echo "\n*** add d2g cert to temporary cert DB"
-# TODO: use the DER generaeted by d2g here
-./add_or_replace_root_cert.sh certdb.tmp marketplace-dev-public-root
-
-echo "\n*** reset trusted marketplace list on device"
+echo "\n*** reset trusted marketplace list on device $device to https://$d2gHostname"
 # TODO : put the host name for the d2g service here!
-./change_trusted_servers.sh $device \
-       "https://marketplace-dev.allizom.org,https://marketplace.allizom.org,https://marketplace.firefox.com"
+./change_trusted_servers.sh $device "https://$d2gHostname"
 
-echo "\n*** push temporary cert DB to device"       
+echo "\n*** push temporary cert DB to device $device"       
 ./push_certdb.sh $device certdb.tmp
