f7213509d0aa28c9ef33cc9a7d7c9797cd4886ac
Bill Walker <bwalker@mozilla.com>
2014-03-07 09:31:23 -0800

    actually report if these missing items are missing

--- check_device.sh
+++ check_device.sh
@@ -19,10 +19,23 @@ adb pull /system/b2g/defaults/pref/user.js user-old.js
 
 #find the crucial line
 grep "dom.mozApps.signed_apps_installable_from" user-old.js
+found=$?
+if [ $found -eq 1 ]; then
+	echo "\n*** this phone does not have a value for dom.mozApps.signed_apps_installable_from in /system/b2g/defaults/pref/user.js"
+fi
 
 # retrieve the certdb
 rm -rf foundcertdb
 ./pull_certdb.sh $device foundcertdb
 
 #look for d2g-public-key
-NSS_DEFAULT_DB_TYPE=sql certutil -L -d foundcertdb
+NSS_DEFAULT_DB_TYPE=sql certutil -L -d foundcertdb > foundcertdb-index.txt
+
+grep "d2g-public-key" foundcertdb-index.txt
+found=$?
+
+if [ $found -eq 1 ]; then
+	echo "\n*** this phone does not have a d2g-public-key in its cert database"
+fi
+
+
